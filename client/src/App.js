67b868770290790ce52ad324a65fcec554487138
Eric Perkins <ericdperkins7@gmail.com>
2022-09-24 15:36:31 -0600

    added NoMatch and integrated MainNavbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+import MainNavbar from './components/MainNavbar'
 import Home from './components/shared/home/Home';
 import Users from './components/users/Users'
 import NoMatch from './components/shared/NoMatch';
 
 const App = () => (
   <>
+    <MainNavbar />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/users' element={<Users />} />
