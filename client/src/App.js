a0d5fc226401b63d608c32bee4bca9b9911bf3a0
Eric Perkins <ericdperkins7@gmail.com>
2022-09-24 15:48:41 -0600

    resolved some conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
-import { Routes, Route } from 'react-router-dom';
-import MainNavbar from './components/MainNavbar'
 import Home from './components/shared/home/Home';
-import Users from './components/users/Users'
-import NoMatch from './components/shared/NoMatch';
+import Nomatch from './components/shared/Nomatch';
+import MainNavbar from './components/shared/MainNavbar';
+import Footer from './components/shared/Footer';
+import Users from './components/users/Users';
+import UserShow from './components/users/UserShow';
+import Doctors from './components/doctors/Doctors';
+import DoctorShow from './components/doctors/DoctorShow';
+// import Appointments from './components/appointments/Appointments';
 
 const App = () => (
   <>
     <MainNavbar />
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/users' element={<Users />} />
-      <Route path='/*' element={<NoMatch />} />
-    </Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/users' element={<Users />} />
+    <Route path='/users/:id' element={<UserShow />} />
+    <Route path='/doctors' element={<Doctors />} />
+    <Route path='/doctors/:id' element={<DoctorShow />} />
+    {/* <Route path='/:courseId/appointments' element={ <Appointments /> } /> */}
+    <Route path='/*' element={<Nomatch />} />
+    <Footer />
   </>
 )
 
