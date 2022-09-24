1dec64fdafba0fed905730e9f5a5b4b0190dbed4
Joy Nichols <joynichols@joys-air.lan>
2022-09-24 16:08:28 -0400

    doctor model routes controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_23_005211) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_24_190503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "specialty"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
